17e055d24db6ac8fdfbb7b6bf97255665055322b
sribysana <sribysana@gmail.com>
2021-05-08 05:44:47 +0530

    git page deployment

--- src/App.js
+++ src/App.js
@@ -9,10 +9,9 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/hat" render={()=>(<div> <h1>Hats page</h1></div>)}/>
-        <Route  path={"/shop"} component={HomePage}> 
-          <Route path={'/shop/hats'} render={()=>(<div> <h1>Hats page</h1></div>)}/>
-          <Route path={'/shop/men'} render={()=>(<div> <h1>mens page</h1></div>)}/>
-        </Route>       
+        <Route  path={"/shop"} render={()=>(<div> <h1>Shops page</h1></div>)} /> 
+        <Route path={'/shop/hats'} render={()=>(<div> <h1>Hats page</h1></div>)}/>
+        <Route path={'/shop/men'} render={()=>(<div> <h1>mens page</h1></div>)}/>               
       </Switch>
       
     </div>
